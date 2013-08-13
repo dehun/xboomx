44efe8e8afc3429a07a0566feebfd3ab270e1621
Yuriy Netesov <973520+dehun@users.noreply.github.com>
2013-08-13 16:40:16 +0300

    some minor changes

--- xboomx/bin/xboomx_sort.py
+++ xboomx/bin/xboomx_sort.py
@@ -1,20 +1,22 @@
 import shelve
 import fileinput
+import os
 
 
 def main():
     # open db
-    db = shelve.open('~/.xboomx/xboomx.db')
+    db = shelve.open(os.getenv("HOME") + '/.xboomx/xboomx.db')
 
     # read lines and set weight according to db
     items = []
 
     for input_item in fileinput.input():
+        input_item = input_item.strip('\n')
         items.append((db.get(input_item, 0), input_item))
 
 
     # sort items
-    items.sort(key= lambda x: x[0])
+    items.sort(key= lambda x: x[0], reverse=True)
 
     # print items
     for item in items:
