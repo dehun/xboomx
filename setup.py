30deb1e7efb04a1c98a9e25ea4794b92af9a5821
Yuriy Netesov retnuhed <Yuriy Netesov retnuhed@gmail.com>
2013-08-14 11:28:26 +0300

    Added license, some changes to setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,18 @@
 from setuptools import setup
 
+
 setup(
     name='xboomx',
     version='0.1dev',
     packages=['xboomx'],
-scripts=['xboomx/bin/xboomx_sort.py',
-         'xboomx/bin/xboomx_update.py',
-         'xboomx/bin/xboomx'],
-license='BSD',
-long_description='wrapper for most common occurences in dmenu',
-install_requires=[]
+    scripts=['xboomx/bin/xboomx_sort.py',
+             'xboomx/bin/xboomx_update.py',
+             'xboomx/bin/xboomx'],
+    license='BSD',
+    long_description='wrapper for most common occurences in dmenu',
+    install_requires=[],
+    include_package_data=True,
+    package_data={'shared': ["etc/config"]},
+    author="Yuriy Netesov",
+    email="retnuhed@gmail.com",
 )
