8a910a07bf65d73d0a72a414daaab3fe65f6f08c
Yuriy Netesov <973520+dehun@users.noreply.github.com>
2014-04-17 06:29:50 -0400

    version ++

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='xboomx',
-    version='0.42',
+    version='0.43',
     packages=['xboomx'],
     scripts=['xboomx/bin/xboomx_path.py',
              'xboomx/bin/xboomx_sort.py',
