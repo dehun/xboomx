a339a3320a6b57ef76ba05359f1fd8141f8390f3
Yuriy Netesov <973520+dehun@users.noreply.github.com>
2014-06-10 09:54:01 -0400

    added web_xboomx with support for firefox urls

--- xboomx/bin/xboomx_update.py
+++ xboomx/bin/xboomx_update.py
@@ -1,14 +1,20 @@
 #!/usr/bin/python
+import sys
 import fileinput
+
 import xboomx.db
 
 
 def main():
+    # get db type
+    db_type = ''
+    if len(sys.argv) > 1:
+        db_type = sys.argv[1]
     # open db
-    db = xboomx.db.open_shelve()
+    db = xboomx.db.open_shelve(db_type)
 
     # get item to update
-    item = fileinput.input().next()
+    item = fileinput.input([]).next()
     item = item.strip('\n')
 
     # update item
